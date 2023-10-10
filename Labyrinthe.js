a5e64994e470388788a51b46156f2b1a23e51f60
Adil GAHRAMANLI <adil.gahramanli@Digital-Grenoble.local>
2023-10-10 13:36:45 +0200

    affichage grille carré simple

---
+++ Labyrinthe.js
@@ -0,0 +1,39 @@
+console.log(data);
+
+// for(i=0; i<= 9; i++) {
+//     // $("#labyrintheCases").append("<div id=case" + i + "></div>")
+//     // Using String literals
+//     $(".labyrintheCases").append(`<div id="case-${i}"></div>`);
+
+// }
+
+
+const labyrintheVar = $("#labyrintheCases");
+
+function labyrintheCreation(variable){
+        for(let i=0; i<=8; i++) {
+        variable.append(`<div id= "case-${i}"></div>`);
+        }
+    }
+
+labyrintheCreation(labyrintheVar);
+
+// const labyrinthe= $("#labyrinthe");
+// function labyrintheCell(variable) {
+//     for(let i=0; i<=3.ex-0[0].walls.length; i++) {
+//         variable.append
+//     }
+
+// }
+
+// data.3.exo-0[0].walls.css("border-top", 1px )
+
+const labyrintheDiv = $("#labyrinthe");
+function labyrintheCell (variable) {
+    for (let i=0; i<=data["3"]["ex-0"].length; i++) {
+        variable.append(
+
+    )
+}
+
+
